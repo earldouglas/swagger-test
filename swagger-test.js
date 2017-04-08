d1748757472fea8f33a9384ba8fc28fbd59cb7a8
James Earl Douglas <james@earldouglas.com>
2017-04-08 07:44:55 -0600

    Show summaries of test results
    * List each test that passed
    * List each test that failed, and show a diff of expected vs. received

--- swagger-test.js
+++ swagger-test.js
@@ -4,6 +4,7 @@ var preq        = require('preq');
 var assert      = require('assert');
 var readline    = require('readline');
 var swaggerTest = require('./lib/swagger-test.js');
+var jsonDiff    = require('json-diff');
 
 var source = [];
 
@@ -17,40 +18,62 @@ rl.on('line', function (line) {
 });
 
 rl.on('close', function () {
-  var fail = false;
+  var passed = [];
+  var failed = [];
+  var requests = [];
   var swaggerSpec = JSON.parse(source.join('\n'));
   var xamples = swaggerTest.parse(swaggerSpec);
-    xamples.forEach(function (xample) {
-    preq[xample.request.method](xample.request).then(function (response) {
-      if (response.status && xample.responses[response.status]) {
-        var xampleResponse = xample.responses[response.status];
-        if (response.headers && xampleResponse.headers) {
-          for (var h in response.headers) {
-            if (response.headers.hasOwnProperty(h)) {
-              if (!(h in xampleResponse.headers)) {
-                delete response.headers[h];
+  xamples.forEach(function (xample) {
+    requests.push(
+      preq[xample.request.method](xample.request).then(function (response) {
+        if (response.status && xample.responses[response.status]) {
+          var xampleResponse = xample.responses[response.status];
+          if (response.headers && xampleResponse.headers) {
+            for (var h in response.headers) {
+              if (response.headers.hasOwnProperty(h)) {
+                if (!(h in xampleResponse.headers)) {
+                  delete response.headers[h];
+                }
               }
             }
           }
+          try {
+            assert.deepEqual(response, xampleResponse);
+            passed.push({
+              description: xample.description,
+            });
+          } catch (e) {
+            failed.push({
+              description: xample.description,
+              expected: e.expected,
+              actual: e.actual,
+            });
+          }
         }
-        try {
-          assert.deepEqual(response, xampleResponse);
-        } catch (e) {
-          fail = true;
-          console.error('response did not match specification');
-          console.error('the specification is:');
-          console.error(JSON.stringify(e.expected, null, 2));
-          console.error('the response was:')
-          console.error(JSON.stringify(e.actual, null, 2));
-        }
-      } else {
-        fail = true;
-        console.error('no x-ample response found');
-      }
-    }).then(function () {
-      if (fail) {
-        process.exit(1);
-      }
+      })
+    );
+  });
+  Promise.all(requests).then(function () {
+
+    console.log(passed.length.toString(), 'tests passed:');
+    passed.forEach(function (x) {
+      console.log('\n  -', x.description);
     });
+    console.log();
+
+    console.error(failed.length.toString(), 'tests failed:');
+    failed.forEach(function (x) {
+      console.error('\n  -', x.description);
+      console.error();
+      var diff = jsonDiff.diff(x.actual, x.expected);
+      JSON.stringify(diff, null, 2).split('\n').forEach(function (x) {
+        console.error('   ', x.replace(/"__old":/g, '"expected":')
+                              .replace(/"__new":/g, '"received":'));
+      });
+    });
+
+    if (failed.length > 0) {
+      process.exit(1);
+    }
   });
 });
